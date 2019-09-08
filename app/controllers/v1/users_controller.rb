4c0eff1e23df295ae9c41e13e1ea32257ed322a3
Theo Morra <td512@td512.io>
2019-09-08 12:14:08 +1200

    basic resources for :users

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -1,3 +1,5 @@
 class V1::UsersController < V1::VersionController
-
+  def index
+    super
+  end
 end
--- app/controllers/v1/version_controller.rb
+++ app/controllers/v1/version_controller.rb
@@ -1,2 +1,6 @@
 class V1::VersionController < ApplicationController
+  def index
+    caller = controller_name.singularize
+    render :json => {"#{caller}_count" => (caller.capitalize.constantize).all.count, :status => :ok}
+  end
 end
