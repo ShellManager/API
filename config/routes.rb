7be763db72f076e72008a804441b17c51195d251
Theo Morra <td512@td512.io>
2019-09-13 22:36:26 +1200

    4xx, 5xx, new scope, namespace
    and comments because I can never remember what resources map to

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
+# frozen_string_literal: true
+
 require 'api_constraints'
 Rails.application.routes.draw do
-  get '/404' => "errors#not_found"
-  get '/500' => "errors#internal_server_error"
-  scope '/api', defaults: { :format => :json } do
+  root 'errors#not_found'
+  get '/400' => 'errors#bad_request'
+  get '/404' => 'errors#not_found'
+  get '/500' => 'errors#internal_server_error'
+
+  scope '/api', defaults: { format: :json } do
     namespace :v1 do
-      resources :users
+      resources :users, except: %i[new edit], :id => /.*/
+      # GET /users     # index
+      # GET /users/:id # show
+      # POST /users    # create
+      # PUT /users/:id # update
+      # DESTROY /users # destroy
     end
   end
 end
