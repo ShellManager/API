c678cd8c133bf32bce93e77e5face6573cec8bad
Theo Morra <td512@td512.io>
2019-09-13 22:34:51 +1200

    Pull bearer from headers
    This is _not_ your standard Bearer header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  def bearer_token
+    pattern = /^Bearer /
+    header  = request.headers["Authorization"]
+    header.gsub(pattern, '') if header&.match(pattern)
+  end
+
 end
