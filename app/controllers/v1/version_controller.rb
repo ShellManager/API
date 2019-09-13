b9fe3bc0413dd90896d7afbf87e6c37642d562f5
Theo Morra <td512@td512.io>
2019-09-13 22:35:41 +1200

    Making super.index available for all controllers

--- app/controllers/v1/version_controller.rb
+++ app/controllers/v1/version_controller.rb
@@ -1,6 +1,9 @@
 class V1::VersionController < ApplicationController
   def index
     caller = controller_name.singularize
-    render :json => {"#{caller}_count" => (caller.capitalize.constantize).all.count, :status => :ok}
+    model = caller.capitalize.constantize
+    render json: { "#{caller}_count" => model.all.count, :status => :ok } if defined? model
+    render json: { status: "Resource #{request.original_fullpath} not found" } unless defined? model
   end
 end
+
