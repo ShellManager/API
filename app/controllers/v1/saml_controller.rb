9f113f9900b4c647ccd71424431bb3184a8b5eab
Theo Morra <td512@td512.io>
2019-11-07 12:10:48 +1300

    This resolves SAML not working. Yay SAML!

--- app/controllers/v1/saml_controller.rb
+++ app/controllers/v1/saml_controller.rb
@@ -1,5 +1,7 @@
 class V1::SamlController < V1::VersionController
 
+  @saml_config = YAML.load_file(File.join(Rails.root, 'config', 'saml.yml'))
+
   def index
     xml_uuid = SecureRandom.uuid
     SamlToken.create!(uuid: xml_uuid, xml: request.original_fullpath)
@@ -12,15 +14,15 @@ class V1::SamlController < V1::VersionController
 
     if @user && xml && @user.saml_enabled
       authn_request, @relay_state = SAML2::Bindings::HTTPRedirect.decode(xml.xml)
-      puts authn_request.valid_interoperable_profile?
-      puts authn_request.assertion_consumer_service_url.location
+      authn_request.resolve(self.class.service_provider)
       response = SAML2::Response.respond_to(authn_request,
                                             SAML2::NameID.new(self.class.entity_id),
                                             self.class.idp_name_email(@user))
       response.sign(self.class.x509_certificate, self.class.private_key)
-      @saml_response = Base64.encode64(response.to_xml)
+      @saml_response = Base64.encode64(response.to_s)
       @saml_acs_url = authn_request.assertion_consumer_service.location
-      render template: "saml2/http_post", layout: false
+      SamlToken.delete(xml.id)
+      render template: "saml2/http_post.html", layout: false
     else
       redirect_to "http://localhost:3001/500"
     end
@@ -32,28 +34,24 @@ class V1::SamlController < V1::VersionController
     SAML2::NameID.new(user.saml_email, SAML2::NameID::Format::UNSPECIFIED)
   end
 
-  def self.saml_config
-    @config ||= YAML.load(File.read(File.join(Rails.root, 'config', 'saml.yml')))
-  end
-
   def self.service_provider
-    @sp ||= SAML2::Entity.parse(File.read(saml_config[:service_provider])).roles.first
+    SAML2::Entity.parse(File.read(File.join(Rails.root, 'config', @saml_config['service_provider']))).service_providers.first
   end
 
   def self.entity_id
-    saml_config[:entity_id]
+    @saml_config['entity_id']
   end
 
   def self.x509_certificate
-    @cert ||= File.read(File.join(Rails.root, 'config', 'keys', saml_config[:encryption][:certificate]))
+    File.read(File.join(Rails.root, 'config', 'keys', @saml_config['encryption']['certificate']))
   end
 
   def self.private_key
-    @key ||= File.read(File.join(Rails.root, 'config', 'keys', saml_config[:encryption][:private_key]))
+    File.read(File.join(Rails.root, 'config', 'keys', @saml_config['encryption']['private_key']))
   end
 
   def self.signature_algorithm
-    saml_config[:encryption][:algorithm]
+    @saml_config['encryption']['algorithm']
   end
 
 
